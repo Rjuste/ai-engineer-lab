8a853d9445056e177f60ef708523097d78fcc23e
Rjuste <rj112220@gmail.com>
2026-08-15 08:34:48 -0400

    Track included and dropped history in context budget

--- src/AIEngineerLab.API/ContextBuildResult.cs
+++ src/AIEngineerLab.API/ContextBuildResult.cs
@@ -2,8 +2,11 @@ public record ContextBuildResult(
     IReadOnlyList<LlmMessage> Messages,
     int EstimatedInputTokens,
     int MaxContextTokens,
-    int ReservedOutputTokens)
+    int ReservedOutputTokens,
+    int TotalHistoryMessages,
+    int IncludedHistoryMessages)
 {
     public int MaxInputTokens => MaxContextTokens - ReservedOutputTokens;
     public int RemainingInputTokens => MaxInputTokens - EstimatedInputTokens;
+    public int DroppedHistoryMessages => TotalHistoryMessages - IncludedHistoryMessages;
 }
